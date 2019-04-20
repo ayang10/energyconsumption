75a1bd73c5c8dd3349632f99781a41edf9545053
ayang10 <ayang014@gmail.com>
2019-04-20 12:06:14 -0400

    create new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Point to controller # the your function, we want this to be the root page
+  root 'get_datas#index'
 end
