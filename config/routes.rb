89a0f4cafefb2f8c3e3c18cce4df15bc1283a481
ayang10 <ayang014@gmail.com>
2019-04-21 06:14:29 -0400

    styling for app

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Point to controller # the your function, we want this to be the root page
-  root 'get_datas#index'
+  root 'get_datas#index', as: 'home'
+
+  get 'about' => 'pages#about'
+
+  resources :get_datas
+
 end
