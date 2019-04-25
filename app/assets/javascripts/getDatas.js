fd2747682f3964a7430613f4e45f53d593a60c0f
ayang10 <ayang014@gmail.com>
2019-04-24 21:46:15 -0400

    finishing up simple energy consumption app

--- app/assets/javascripts/getDatas.js
+++ app/assets/javascripts/getDatas.js
@@ -4,49 +4,25 @@ document.addEventListener('DOMContentLoaded', function(event) {
     data: function() {
         return {
             search: '',
-            sort: {
-                field: '',
-                desc: true
-            },
+            sort: '',
             listProperties: [ 
                 { address: '123 Pond rd', city: 'Fish', state: 'MC', zip: '23458', 
-                energy: [{date: 1/1/2019, energyUsed: 56}, {date: 1/2/2019, energyUsed: 49}, {date: 1/3/2019, energyUsed: 23}
+                energy: [{date: '1/1/2019', energyUsed: 56}, {date: '1/2/2019', energyUsed: 49}, {date: '1/3/2019', energyUsed: 23}
                 ]},
                 { address: '435 King st', city: 'Queen', state: 'SU', zip: '43664', 
-                energy: [{date: 1/1/2019, energyUsed: 100}, {date: 1/2/2019, energyUsed: 123}, {date: 1/3/2019, energyUsed: 115}
+                energy: [{date: '1/1/2019', energyUsed: 100}, {date: '1/2/2019', energyUsed: 123}, {date: '1/3/2019', energyUsed: 115}
                 ]},
                 { address: '345 Crack rd', city: 'Smoke', state: 'PO', zip: '34678', 
-                energy: [{date: 1/1/2019, energyUsed: 89}, {date: 1/2/2019, energyUsed: 85}, {date: 1/3/2019, energyUsed: 89}
+                energy: [{date: '1/1/2019', energyUsed: 89}, {date: '1/2/2019', energyUsed: 85}, {date: '1/3/2019', energyUsed: 89}
                 ]},
                 { address: '123 Mushroom rd', city: 'Boy', state: 'SP', zip: '34568', 
-                energy: [{date: 1/1/2019, energyUsed: 25}, {date: 1/2/2019, energyUsed: 80}, {date: 1/3/2019, energyUsed: 100}
+                energy: [{date: '1/1/2019', energyUsed: 25}, {date: '1/2/2019', energyUsed: 80}, {date: '1/3/2019', energyUsed: 100}
                 ]}
             ]
         }
     },
     mounted: function() {
-        const data = {
-    labels: ["12am-3am", "3am-6pm", "6am-9am", "9am-12am",
-        "12pm-3pm", "3pm-6pm", "6pm-9pm", "9am-12am"
-    ],
-    datasets: [
-        {
-            name: "Some Data", type: "bar",
-            values: [25, 40, 30, 35, 8, 52, 17, -4]
-        },
-        {
-            name: "Another Set", type: "line",
-            values: [25, 50, -10, 15, 18, 32, 27, 14]
-        }
-    ]
-}
-        new frappe.Chart("#chart", {  // or a DOM element,
-            title: "Energy Consumption",
-            data: data,
-            type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
-            height: 250,
-            colors: ['#7cd6fd', '#743ee2']
-        })
+       this.createChart();
     },
     computed: {
         searchFilter: function() {
@@ -63,12 +39,49 @@ document.addEventListener('DOMContentLoaded', function(event) {
             }
             return total;
         },
-        sortEnergyAvg: function(event){ console.log('here')
-            if(event.target.value == 'asc'){
-                !this.searchFilter.desc
+        createChart: function(){
+            let newArrDatas = [];
+      
+            if (this.searchFilter.length > 0){
+                let newLabels = this.searchFilter[0].energy.map(a => a.date);
+
+                for (let i = 0; i < this.searchFilter.length; i++){
+                    newArrDatas.push({name: this.listProperties[i].address}, {type: "line"}, {values: this.searchFilter[i].energy.map(a => a.energyUsed)})
+                }
+                
+                const data = {
+                    labels: newLabels,
+                    datasets: newArrDatas
+                }
+                new frappe.Chart("#chart", {  // or a DOM element,
+                    title: "Energy Consumption",
+                    data: data,
+                    type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
+                    height: 250,
+                    colors: ['#7cd6fd', '#743ee2']
+                })
             }
             else {
-                this.searchFilter.desc
+                new frappe.Chart("#chart", {  // or a DOM element,
+                    title: "Energy Consumption",
+                    data: {labels: "", datasets: []},
+                    type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
+                    height: 250,
+                    colors: ['#7cd6fd', '#743ee2']
+                })
+            }
+        }
+    },
+    watch: {
+        search: function(){
+            this.createChart();
+        },
+        sort: function(val) {
+            if(val == 'asc'){
+                this.searchFilter = this.searchFilter.sort((a,b) => this.getEnergyAvg(a.energy) - this.getEnergyAvg(b.energy));
+            }
+            else {
+                this.searchFilter = this.searchFilter.sort((a,b) => this.getEnergyAvg(b.energy) - this.getEnergyAvg(a.energy));
             }
         }
     }
